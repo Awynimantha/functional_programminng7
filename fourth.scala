bc6a749b070b6e4b532bf16f4c48ce0f5843b436
Nimantha <Awynimantha@gmail.com>
2022-09-12 19:55:14 +0530

    second

---
+++ fourth.scala
@@ -0,0 +1,51 @@
+
+object first_question extends App{
+    class Account(ni:String,n:Int,b:Double){
+        val nic:String=ni
+        val acnumber:Int=n
+        var balance:Double=b
+
+        override def toString="Account deatils\n"+"NIC :"+nic+"\n"+"Account no. :"+acnumber+"\n"+"Balance :"+balance
+        def transfer(amount:Double,that:Account):Account={
+            if (this.balance-amount < 0) {
+                print("No enough money to transfer")
+                new Account(that.nic,that.acnumber,that.balance)
+            }
+            else{
+                this.balance=this.balance-amount
+                new Account(that.nic,that.acnumber,that.balance+amount)
+            }
+        }
+    }
+
+    
+    // first function
+    def isNeg(acc:Account):Boolean= acc.balance match{
+            case x if x<0 => true
+            case _ =>false
+
+    }
+    def findNeg=(l:List[Account])=>l.filter(isNeg(_))
+
+
+    // second function
+   def findSum=(l:List[Account])=>l.map(_.balance).reduce((x,y)=>x+y)
+
+   
+    //third function
+    def calcInterest(acc:Account):Account= acc.balance match{
+        case x if x>0 => new Account(acc.nic,acc.acnumber,acc.balance * 1.05)
+        case _ =>new Account(acc.nic,acc.acnumber,acc.balance * 1.1)
+    }
+    
+     def finalSum=(l:List[Account])=>l.map(calcInterest(_)).map((x)=>x.balance).reduce((x,y)=>x+y)
+
+     
+    //function calls
+    val accList=List(new Account("2323",343,-100),new Account("4343",344,-100),new Account("5454",345,190))
+  
+    println("Negative balance accounts :"+findNeg(accList).map((x)=>x.acnumber))
+    println("Total of all account balances :"+findSum(accList))
+    print("Total of all accounts after adding interest :")
+    println("%.2f".format(finalSum(accList)))
+}
